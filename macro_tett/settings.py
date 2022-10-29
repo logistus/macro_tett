defcea4352b984adf53ba63f553c8a0bab781009
Sinan <logistus@gmail.com>
2022-10-30 00:14:04 +0300

    setting.py updated

--- macro_tett/settings.py
+++ macro_tett/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-25-6a1b-+g+ri6!b&5rx$+z^11(bkc!51o*x6@y%)sg$yf$@@j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['188.166.34.7']
 
 
 # Application definition
