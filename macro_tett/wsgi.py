ad0cbe2089c37a6c2be96a26660cfaeab5983917
Sinan <logistus@gmail.com>
2022-11-02 01:11:13 +0300

    wsgi.py updated

--- macro_tett/wsgi.py
+++ macro_tett/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
+from pathlib import Path
+import environ
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'macro_tett.settings.prod')
+BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', env('SETTINGS_MODULE'))
 
 application = get_wsgi_application()
