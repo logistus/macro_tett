630b35e1917634096faee8e7ce585d556b1c7fab
Sinan <logistus@gmail.com>
2022-11-01 22:51:29 +0300

    settings files updated

--- macro_tett/wsgi.py
+++ macro_tett/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'macro_tett.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'macro_tett.settings.prod')
 
 application = get_wsgi_application()
