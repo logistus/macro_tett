ea236181b786864414288a1e87c037ff5bc99fd7
Sinan <logistus@gmail.com>
2022-11-02 00:29:02 +0300

    settings prod updated

--- macro_tett/settings_prod.py
+++ macro_tett/settings_prod.py
@@ -65,8 +65,11 @@ WSGI_APPLICATION = 'macro_tett.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'macro_tett',
+        'USER': 'logistus',
+        'PASSWORD': env('DB_PASS'),
+        'PORT': ''
     }
 }
 
