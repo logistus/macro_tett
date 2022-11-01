36f04a6d2eff556cb74e07e88b8b09eeeaf054cc
Sinan <logistus@gmail.com>
2022-11-02 00:50:31 +0300

    manage.py updated

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
