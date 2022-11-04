eca0c0d53e186240472d452ef53fa96e08e9df0a
Sinan <logistus@gmail.com>
2022-11-04 12:28:28 +0300

    updated

--- products/models.py
+++ products/models.py
@@ -1,10 +1,16 @@
+import os
 from django.db import models
+from django.conf import settings
 from django_resized import ResizedImageField
 
 
 class Product(models.Model):
     product_image = ResizedImageField(scale=0.5, upload_to='products')
     expiry_date = models.DateField()
+
+    def delete(self, *args):
+        os.remove(os.path.join(settings.MEDIA_ROOT, self.product_image.name))
+        super().delete()
 
     def __str__(self):
         return str(self.expiry_date)
