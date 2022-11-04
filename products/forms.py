ce655b68527bf8045494fa36ef0371334f3ccb06
Sinan <logistus@gmail.com>
2022-11-04 15:55:26 +0300

    updated

--- products/forms.py
+++ products/forms.py
@@ -5,11 +5,12 @@ from products.models import Product
 class CreateProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['product_image', 'expiry_date']
+        fields = ['product_image', 'expiry_date', 'is_macropicks']
         widgets = {
             'expiry_date': forms.DateInput(format=('%Y-%m-%d'), attrs={'type': 'date'}),
         }
         labels = {
             'product_image': 'Ürün Fotoğrafı',
             'expiry_date': 'TETT',
+            'is_macropicks': 'Macropicks'
         }
